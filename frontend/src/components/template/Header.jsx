b645cbe08d4b85bad976320ac744b4fb23229600
Julio Cesar <julio.souza7@hotmail.com>
2023-01-17 17:25:25 -0300

    first commit

---
+++ frontend/src/components/template/Header.jsx
@@ -0,0 +1,6 @@
+import "./Header.css";
+import React from "react";
+
+export default function logo(props) {
+  return <aside className="logo">Cabeçalho</aside>;
+}
---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,7 @@
+import './Nav.css'
+import React from 'react'
+
+export default function Nav(props) {
+    return <aside className="menu-area">Navegação</aside>;
+  }
+  
